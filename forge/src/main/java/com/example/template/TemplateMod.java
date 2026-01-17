6a22b11dd5238f04331f1f442dbfb4aa034c32b0
shiroha <2596998506@qq.com>
2026-01-17 10:13:49 +0800

    首次

---
+++ forge/src/main/java/com/example/template/TemplateMod.java
@@ -0,0 +1,14 @@
+package com.example.template;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TemplateMod.MOD_ID)
+public class TemplateMod {
+
+    public static final String MOD_ID = "template";
+
+    public TemplateMod() {
+        // Forge mod constructor
+        System.out.println("Initializing Template Mod (Forge)...");
+    }
+}
